f5a85d7347a4fa974800047786b3ac1b67cb0bac
Marcus Souza <marcussouza@CS0579.local>
2025-10-15 16:26:37 -0300

    create tags

--- provider.tf
+++ provider.tf
@@ -1,12 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.16.0"
     }
+  }
+  backend "s3" {
+    bucket = "comunity-ns-eks"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
+
